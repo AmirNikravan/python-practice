5ed45d33128d584d381df64dc27efaa9528d5260
AmirNikravan <amirnikravan@outlook.com>
2023-03-06 21:45:33 +0330

    df

--- Hack/hashmaker.py
+++ Hack/hashmaker.py
@@ -2,3 +2,9 @@ def make_hash():
     with open("/tmp/sha256.txt","w") as file:
         for i in range (0,10000):
             file.write(f"{i} = {str(hash(str(i).zfill(4)))}\n")
+def hashtodict():
+    with open("/tmp/sha256.txt") as file:
+        passes = dict()
+        for i in range(0,10000):
+            passes[i]= file.readline().strip()
+        return passes
