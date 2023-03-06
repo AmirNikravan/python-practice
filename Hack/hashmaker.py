676baf3a1e6e8b96c7f27d3abcc7f41a45550556
AmirNikravan <amirnikravan@outlook.com>
2023-03-06 22:00:45 +0330

    create dict with hash key

--- Hack/hashmaker.py
+++ Hack/hashmaker.py
@@ -1,10 +1,12 @@
 def make_hash():
-    with open("/tmp/sha256.txt","w") as file:
-        for i in range (0,10000):
+    with open("/tmp/sha256.txt", "w") as file:
+        for i in range(0, 10000):
             file.write(f"{i} = {str(hash(str(i).zfill(4)))}\n")
+
+
 def hashtodict():
     with open("/tmp/sha256.txt") as file:
         passes = dict()
-        for i in range(0,10000):
-            passes[i]= file.readline().strip()
-        return passes
+        for i in range(0, 19):
+            passes[file.readline()[4:].strip()] = i
+        print( passes)
