2c894fe3f76bf4a0d9d03a686d80c9502629e0cf
AmirNikravan <amirnikravan@outlook.com>
2023-03-10 21:01:07 +0330

    create square.py that draws a square with turtle

---
+++ Turtle/square.py
@@ -0,0 +1,12 @@
+import turtle
+
+squre = turtle.Turtle()
+squre.forward(100)
+squre.left(90)
+squre.forward(100)
+squre.left(90)
+squre.forward(100)
+squre.left(90)
+squre.forward(100)
+screen = turtle.Screen()
+screen.exitonclick()
