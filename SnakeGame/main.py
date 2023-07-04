10915af532232b9a3a1b152542b724467b447f34
Amir <amirnirkavan@outlook.com>
2023-07-04 16:35:45 +0330

    add filr for high score

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -22,17 +22,17 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    # score.show()
+    score.show()
     if snake.head.distance(food) < 15:
         food.refresh()
         score.increase()
         snake.extend()
     if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
     for segments in snake.segments[1:]:
         if snake.head.distance(segments) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 screen.exitonclick()
   
--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -8,14 +8,26 @@ class Score(Turtle):
         self.color("white")
         self.hideturtle()
         self.penup()
-        self.goto(0,270)
+        self.goto(0, 270)
+        with open("E:\Programming\Python\python-practice\SnakeGame\data.txt") as data:
+            self.high_score = int(data.read())
+
     def increase(self):
         self.score += 1
         self.show()
+
     def show(self):
         self.clear()
-        self.write(f"score = {self.score}")
+        self.write(f"score = {self.score} high score = {self.high_score}")
+
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER")
-    
+
+    def reset(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+            with open("E:\Programming\Python\python-practice\SnakeGame\data.txt",mode='w') as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.show()
